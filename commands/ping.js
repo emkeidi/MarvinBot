9bf2463c018e6a60112510bd82254f190c7675b3
Matt Davis <hello.mattdavis@gmail.com>
2023-02-23 21:22:53 -0500

    ping server user commands

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		interaction.editReply(
+			`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`,
+		);
+		await interaction.followUp({
+			content: 'Oh, are we playing a game? *sigh*',
+			ephemeral: true,
+		});
+		await wait(4000);
+		await interaction.followUp({
+			content: `Alright, here's the roundtrip latency again: ${
+				sent.createdTimestamp - interaction.createdTimestamp
+			}ms`,
+			ephemeral: true,
+		});
 	},
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -23,8 +23,17 @@ for (const file of commandFiles) {
 	}
 }
 
-client.once(Events.ClientReady, (c) => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.login(token);
