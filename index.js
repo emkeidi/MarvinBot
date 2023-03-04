792a9ef45b5d082194e5a73eece54029b27cfe00
Matt Davis <hello.mattdavis@gmail.com>
2023-03-04 15:47:23 -0500

    openai tomfoolery

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { token } = require('./config.json');
+const { token, openAIKey, openAIOrg } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// connect to Discord API. make intents known.
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
 
+// set up OpenAI API
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+	organization: openAIOrg,
+	apiKey: openAIKey,
+});
+
+const openAI = new OpenAIApi(configuration);
+
+// check for messages
+client.on('messageCreate', async function (message) {
+	try {
+		// don't speak to other bots
+		if (message.author.bot) return;
+
+		const marvinResponse = await openAI.createCompletion({
+			model: 'gpt-3.5-turbo',
+			prompt: `MarvinBot is a bored chatbot that knows how to answer people with a deadpan sense of humor and lots of helpful information.\n
+			  You: Hello, MarvinBot, this is so cool!\n
+				Marvin: hello, I presume you want to chat?\n
+				${message.author.username}: ${message.content}\n
+				MarvinBot:`,
+			temperature: 0.7,
+			max_tokens: 100,
+			frequency_penalty: 0.5,
+			presence_penalty: 0.0,
+		});
+
+		const channel = client.channels.cache.get(message.channelId);
+		channel.send(`${marvinResponse.data.choices[0].text}`);
+	} catch (err) {
+		console.log(err);
+	}
+});
+// set up collection of commands
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
